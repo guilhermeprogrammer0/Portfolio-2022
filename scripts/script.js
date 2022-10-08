7da8345fab7fee795b876d4982e3bbc9ceb33c3b
Guilherme Souza <110481277+guilhermeprogrammer0@users.noreply.github.com>
2022-10-08 09:35:41 -0300

    Atualizei o script

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 var abrir_menu = document.querySelector("#abrir_menu");
 var navegacao = document.querySelector("#navegacao");
 abrir_menu.addEventListener('click', menu_abrir);
-function menu_abrir() {
+abrir_menu.addEventListener('touchstart',menu_abrir);
+function menu_abrir(event) {
+    if(event.type == 'touchstart') event.preventDefault();
    navegacao.classList.toggle('ativo');
 
 }
