89cd60bc863a89b6a3c266e09e215ce1a3fc0174
Guilherme Souza <110481277+guilhermeprogrammer0@users.noreply.github.com>
2022-10-14 19:31:48 -0300

    Atualizei Projeto

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,15 @@
 var abrir_menu = document.querySelector("#abrir_menu");
 var navegacao = document.querySelector("#navegacao");
 abrir_menu.addEventListener('click', menu_abrir);
-abrir_menu.addEventListener('touchstart',menu_abrir);
+abrir_menu.addEventListener('touchstart', menu_abrir);
 function menu_abrir(event) {
-    if(event.type == 'touchstart') event.preventDefault();
-   navegacao.classList.toggle('ativo');
+    if (event.type == 'touchstart') event.preventDefault();
+    navegacao.classList.toggle('ativo');
 
 }
 var back_top = document.querySelector("#back_top");
-back_top.addEventListener('click',voltar_topo);
+back_top.addEventListener('click', voltar_topo);
 
-function voltar_topo()
-{
-    window.scrollTo(0,0);
+function voltar_topo() {
+    window.scrollTo(0, 0);
 }
